48d09f8612970b9f4abe8d39724cd61b610deac7
Erik Kotsiuba <ErikKotsiuba@udelphi.com>
2016-06-22 17:53:25 +0300

    audio player slider

--- Vinylizer/Scripts/app/app.js
+++ Vinylizer/Scripts/app/app.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(document).ready(function () {
+    console.log(1);
+    var trackName = getCookie("VinylizerFileName");
+
+    $("#filterVolume").change(function () {
+        console.log(trackName, this.value);
+
+        var audio = $("#audio");
+        $("#mp3_src").attr("src", "/Home/GetFilterForPlay?volumeLvl=" + this.value + "&fileName=" + trackName);
+        audio[0].pause();
+        audio[0].load();//suspends and restores all audio element
+
+        //audio[0].play(); changed based on Sprachprofi's comment below
+        audio[0].oncanplaythrough = audio[0].play();
+    });
+
+});
+
+
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
